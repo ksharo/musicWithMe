fb110abb53479473da2e3fc7be25d90671f8d4e5
Jack Schneiderhan <jschnei4@stevens.edu>
2022-04-05 16:10:26 -0400

    added lessons page!

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const trebleRoutes = require('./trebleApi');
 const bassRoutes = require('./bassApi');
 const mainRoutes = require('./mainApi');
+const lessonRoutes = require('./lessonsApi');
 
 const constructorMethod = (app) => {
   app.use('/', mainRoutes);
   app.use('/treble', trebleRoutes);
   app.use('/bass', bassRoutes);
+  app.use('/lessons', lessonRoutes);
   app.use('*', (_, res) => {
     return res.render('individualPages/error', {error: {message: "Page not found.", status: 404}});
   });
