47edcd9fb6a829dcf3ad828c33c6c2d53a5a0130
lrechler <lfoxx328@me.com>
2022-04-06 14:08:43 -0400

    Added create account/login/view account

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ const trebleRoutes = require('./trebleApi');
 const bassRoutes = require('./bassApi');
 const mainRoutes = require('./mainApi');
 const lessonRoutes = require('./lessonsApi');
+const accountRoutes = require('./accountApi');
 
 const constructorMethod = (app) => {
   app.use('/', mainRoutes);
   app.use('/treble', trebleRoutes);
   app.use('/bass', bassRoutes);
   app.use('/lessons', lessonRoutes);
+  app.use('/account', accountRoutes);
   app.use('*', (_, res) => {
     return res.render('individualPages/error', {error: {message: "Page not found.", status: 404}});
   });
