8e9734cfc44d69ab05eb781d0ba2a50b59f61d8b
Jack Schneiderhan <jschnei4@stevens.edu>
2022-04-10 23:02:46 -0400

    added very basic nav bar functionality

--- public/scripts/mainScript.js
+++ public/scripts/mainScript.js
@@ -691,6 +691,14 @@ function initializeHidden() {
     }
 }
 
+function openNav(){
+    document.getElementById("sideNav").style.width = "250px";
+}
+
+function closeNav(){
+    document.getElementById("sideNav").style.width = "0";
+}
+
 startCountOff();
 window.addEventListener("load", function() {
     initializeHidden();
